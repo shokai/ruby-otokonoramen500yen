1fba8e132860ed9ab26e9c4b0d52a63153c32cd4
Sho Hashimoto <hashimoto@shokai.org>
2011-04-28 08:26:46 +0900

    return mail magazine url

--- lib/otokonoramen500yen.rb
+++ lib/otokonoramen500yen.rb
@@ -4,10 +4,14 @@ require 'open-uri'
 require 'kconv'
 
 module Otokonoramen500yen
-  VERSION = '0.0.1'
+  VERSION = '0.0.2'
+
+  def Otokonoramen500yen.url
+    'http://bn.merumo.ne.jp/backno/latestView.do?magId=00589575'
+  end
 
   def Otokonoramen500yen.get
-    page = open('http://bn.merumo.ne.jp/backno/latestView.do?magId=00589575').read.toutf8
+    page = open(url).read.toutf8
     doc = Hpricot(page)
     body = doc.search('//div[@class="bn_content_text"]').inner_text
     date = doc.search('//div[@class="bn_head"]').inner_text
@@ -16,14 +20,15 @@ module Otokonoramen500yen
                    date.scan(/(\d+)日/).first.first,
                    date.scan(/(\d+)時/).first.first,
                    date.scan(/(\d+)分/).first.first)
-    Response.new(body,date)
+    Response.new(body,date,url)
   end
 
   class Otokonoramen500yen::Response
-    attr_reader :body, :date
-    def initialize(body,date)
+    attr_reader :body, :date, :url
+    def initialize(body,date,url)
       @body = body
       @date = date
+      @url = url
     end
   end
 end
